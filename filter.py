15b89c7e9af52e3178a29791292120f8210a0159
kuragechan14 <kuragechan14@gmail.com>
2018-05-14 12:35:40 +0000

    Update low-pass filter

--- filter.py
+++ filter.py
@@ -1,15 +1,15 @@
+import time
 import Adafruit_BMP.BMP085 as BMP085
 sensor = BMP085.BMP085()
 
 last_y=0
 
-def losspass(a,x,last_y):
-	y=a*x+(1-a)*last_y
-	return y
+def lowpass(x,last_y):
+	return 0.5 * x + 0.5 * last_y
 
 while True:
-	x=sensor.read_altitude
-	y=losspass(0.5,x,last_y)
+	x=sensor.read_altitude()
+	y=lowpass(x,last_y)
 	print('(Low-pass Filter)Altitude={0:0.2f} m'.format(y))
 	last_y=y
 	time.sleep(0.5)
